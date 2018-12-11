e9d40370eb112ea0b27f1c5593bb77615eeebb2d
Johan J <johan@staaker.com>
2018-12-11 12:49:04 +0100

    CMake: Update buildscript files for latest version

--- filament/src/Renderer.cpp
+++ filament/src/Renderer.cpp
@@ -276,7 +276,6 @@ void FRenderer::renderJob(ArenaScope& arena, FView& view,
     }
     FrameGraphResource output = fg.importResource("viewRenderTarget",
                                                   { .viewport = vp }, viewRenderTarget, vp.width, vp.height);
-    ColorPass::renderColorPass(engine, js,
 
     /*
      * Depth + Color passes
